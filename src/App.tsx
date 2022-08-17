dba16c9609451031cdca6648a54853f374508220
Beifei Zhou <z.beifei@gmail.com>
2022-08-17 17:28:47 +0100

    Add pin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 // import './App.css'
 import './App.less'
@@ -9,27 +9,7 @@ function App() {
 
   return (
     <div className="app">
-      <MapDisplay className="map-container"/>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <MapDisplay />
     </div>
   )
 }
--- src/MapDisplay.jsx
+++ src/MapDisplay.jsx
@@ -1,22 +1,87 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useMemo } from 'react';
 import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+// import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import './MapDisplay.less'
+import Map, {
+    Marker,
+    Popup,
+    NavigationControl,
+    FullscreenControl,
+    ScaleControl,
+    GeolocateControl
+} from 'react-map-gl';
+import Pin from './pin';
+import CITIES from './data/cities.json';
+import ControlPanel from './ControlPanel';
 
 export const MapDisplay = () => {
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const [popupInfo, setPopupInfo] = useState(null);
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-    });
-    return <div ref={mapContainer} />
+    const pins = useMemo(
+        () =>
+            CITIES.map((city, index) => (
+                <Marker
+                    key={`marker-${index}`}
+                    longitude={city.longitude}
+                    latitude={city.latitude}
+                    anchor="bottom"
+                    onClick={e => {
+                        // If we let the click event propagates to the map, it will immediately close the popup
+                        // with `closeOnClick: true`
+                        e.originalEvent.stopPropagation();
+                        setPopupInfo(city);
+                    }}
+                >
+                    <Pin />
+                    {/* <div>I am here!</div> */}
+                </Marker>
+            )),
+        []
+    );
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
+
+
+    return <div className='map-screen'>
+        {/* <div className='top-map'>
+            <div className='sidebar'>zoom: 9</div>
+        </div> */}
+        <Map
+            initialViewState={{
+                latitude: 40,
+                longitude: -100,
+                zoom: 3.5,
+                bearing: 0,
+                pitch: 0
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            mapboxAccessToken={mapboxgl.accessToken}
+        >
+            <GeolocateControl position="top-left" />
+            {/* <FullscreenControl position="top-left" /> */}
+            {/* <NavigationControl position="top-left" /> */}
+            <ScaleControl />
+            {pins}
+            {popupInfo && (
+                <Popup
+                    anchor="top"
+                    longitude={Number(popupInfo.longitude)}
+                    latitude={Number(popupInfo.latitude)}
+                    onClose={() => setPopupInfo(null)}
+                >
+                    <div>
+                        {popupInfo.city}, {popupInfo.state} |{' '}
+                        <a
+                            target="_new"
+                            href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
+                        >
+                            Wikipedia
+                        </a>
+                    </div>
+                    <img width="100%" src={popupInfo.image} />
+                </Popup>
+            )}
+        </Map>
+        {/* <ControlPanel className='control-panel'/> */}
+    </div>
 }
