15a906e492f55b8e32a022b3f0deb5501ed0245c
Beifei Zhou <z.beifei@gmail.com>
2022-08-18 15:04:06 +0100

    use mapbox native

--- src/MapDisplay.jsx
+++ src/MapDisplay.jsx
@@ -1,87 +1,59 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react';
+import React, { useEffect, useRef, useState, useMemo, useCallback } from 'react';
 import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
-// import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import './MapDisplay.less'
-import Map, {
-    Marker,
-    Popup,
-    NavigationControl,
-    FullscreenControl,
-    ScaleControl,
-    GeolocateControl
-} from 'react-map-gl';
-import Pin from './pin';
-import CITIES from './data/cities.json';
-import ControlPanel from './ControlPanel';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 
 export const MapDisplay = () => {
-    const [popupInfo, setPopupInfo] = useState(null);
-
-    const pins = useMemo(
-        () =>
-            CITIES.map((city, index) => (
-                <Marker
-                    key={`marker-${index}`}
-                    longitude={city.longitude}
-                    latitude={city.latitude}
-                    anchor="bottom"
-                    onClick={e => {
-                        // If we let the click event propagates to the map, it will immediately close the popup
-                        // with `closeOnClick: true`
-                        e.originalEvent.stopPropagation();
-                        setPopupInfo(city);
-                    }}
-                >
-                    <Pin />
-                    {/* <div>I am here!</div> */}
-                </Marker>
-            )),
-        []
-    );
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
 
-    return <div className='map-screen'>
-        {/* <div className='top-map'>
-            <div className='sidebar'>zoom: 9</div>
-        </div> */}
-        <Map
-            initialViewState={{
-                latitude: 40,
-                longitude: -100,
-                zoom: 3.5,
-                bearing: 0,
-                pitch: 0
-            }}
-            mapStyle="mapbox://styles/mapbox/streets-v11"
-            mapboxAccessToken={mapboxgl.accessToken}
-        >
-            <GeolocateControl position="top-left" />
-            {/* <FullscreenControl position="top-left" /> */}
-            {/* <NavigationControl position="top-left" /> */}
-            <ScaleControl />
-            {pins}
-            {popupInfo && (
-                <Popup
-                    anchor="top"
-                    longitude={Number(popupInfo.longitude)}
-                    latitude={Number(popupInfo.latitude)}
-                    onClose={() => setPopupInfo(null)}
-                >
-                    <div>
-                        {popupInfo.city}, {popupInfo.state} |{' '}
-                        <a
-                            target="_new"
-                            href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
-                        >
-                            Wikipedia
-                        </a>
-                    </div>
-                    <img width="100%" src={popupInfo.image} />
-                </Popup>
-            )}
-        </Map>
-        {/* <ControlPanel className='control-panel'/> */}
+    const geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        placeholder: 'Search for a location',
+        marker: false,
+    })
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+        map.current.addControl(geocoder);
+        map.current.addControl(new mapboxgl.NavigationControl());
+        map.current.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+        }));
+        map.current.addControl(new mapboxgl.ScaleControl({ maxWidth: 80, unit: 'metric' }));
+        map.current.addControl(new mapboxgl.FullscreenControl());
+        const marker1 = new mapboxgl.Marker({color: 'red'})
+            .setLngLat([lng, lat])
+            .addTo(map.current);
+
+        // Create a default Marker, colored black, rotated 45 degrees.
+        // const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+        //     .setLngLat([12.65147, 55.608166])
+        //     .addTo(map.current);
+    });
+
+
+
+    return <div className='screen'>
+        <div className='map-container' ref={mapContainer} />
     </div>
 }
