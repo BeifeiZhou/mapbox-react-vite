ea6cdb0c44338370dc768a01a788e0df09425591
Beifei Zhou <z.beifei@gmail.com>
2022-08-19 12:25:30 +0100

    get bbox

--- src/MapDisplay.jsx
+++ src/MapDisplay.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react';
+import React, { useEffect, useRef, useState, useMemo, useCallback } from 'react';
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import "./mapbox-gl-geocoder.css";
 import './MapDisplay.less'
-import Map, {
+import MapGL, {
     Marker,
     Popup,
     NavigationControl,
@@ -18,6 +18,9 @@ import GeocoderControl from './geocoder-control';
 
 export const MapDisplay = () => {
     const [popupInfo, setPopupInfo] = useState(null);
+    const mapRef = useRef();
+    const [bbox, setBbox] = useState([]);
+    const [imgSize, setImgSize] = useState([]);
 
     const pins = useMemo(
         () =>
@@ -43,11 +46,48 @@ export const MapDisplay = () => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
 
 
+    const measure = (lat1, lon1, lat2, lon2) => {
+        // generally used geo measurement function
+        var R = 6378.137; // Radius of earth in KM
+        var dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+        var dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos((lat1 * Math.PI) / 180) *
+            Math.cos((lat2 * Math.PI) / 180) *
+            Math.sin(dLon / 2) *
+            Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = R * c;
+        return d * 1000; // meters
+    }
+
+
+    const getFromMap = () => {
+        let bound = mapRef.current.getBounds();
+        let top = bound._ne.lat;
+        let bottom = bound._sw.lat;
+        let left = bound._sw.lng;
+        let right = bound._ne.lng;
+
+        let imgWidth = document.querySelector(".mapboxgl-map").clientWidth;
+        let imgHeight = document.querySelector(".mapboxgl-map").clientHeight;
+
+        imgHeight = Math.round(measure(top, 1, bottom, 1));
+        imgWidth = Math.round(measure(1, left, 1, right));
+
+        setBbox([left, bottom, right, top]);
+        setImgSize([imgWidth, imgHeight]);
+        console.log('bbox: ', bbox)
+        console.log('imgSize: ', imgSize)
+    }
+
+
     return <div className='map-screen'>
-        {/* <div className='top-map'>
-            <div className='sidebar'>zoom: 9</div>
-        </div> */}
-        <Map
+        <div className='top-map'>
+            <div className='sidebar' onClick={getFromMap}>zoom: 9</div>
+        </div>
+        <MapGL
             initialViewState={{
                 latitude: 40,
                 longitude: -100,
@@ -55,6 +95,8 @@ export const MapDisplay = () => {
                 bearing: 0,
                 pitch: 0
             }}
+            // onViewportChange={handleGeocoderViewportChange}
+            ref={mapRef}
             mapStyle="mapbox://styles/mapbox/streets-v11"
             mapboxAccessToken={mapboxgl.accessToken}
         >
@@ -83,7 +125,7 @@ export const MapDisplay = () => {
                     <img width="100%" src={popupInfo.image} />
                 </Popup>
             )}
-        </Map>
+        </MapGL>
         {/* <ControlPanel className='control-panel'/> */}
     </div>
 }
